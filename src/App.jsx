5c015322561fb08cbab55765c5e2704bd1f42db0
P1ARB33 <parevalo@academic.rrc.ca>
2026-06-23 13:46:13 -0500

    feat: Data passed through Entry component and rendered on App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import Header from "./components/Header"
+import Entry from "./components/Entry"
+import data from "./data"
 
 export default function App() {
+    const entryElements = data.map((entry) => {
+        return (
+            <Entry 
+                key={entry.id}
+                entry={entry}
+            />
+        )
+    })
+
     return (
-        <Header />
+        <>
+            <Header />
+            <main className="container">
+                {entryElements}
+            </main>
+        </>
     )
 }
